919d0488a7674fb7b94bd611e5a96cc643d079a4
hazel-hernandez022 <hazel.perez79@unach.mx>
2023-10-29 18:57:59 -0600

    Agregar archivos de proyecto.

---
+++ Practica11-Ejercicios de Recursividad e Iteración-Python/Practica11_Ejercicios_de_Recursividad_e_Iteración_Python.py
@@ -0,0 +1,10 @@
+#practica11
+#perez hernandez hazel yorelly
+
+print("tabla de multiplicar:)")
+print("______________________")
+NUM = int(input("Digite un numero: "))
+for I in range(1, 13):
+    RESULT = NUM * I
+    print(f"{NUM} * {I} = {RESULT}")
+input("Pulse cualquier tecla: ")
